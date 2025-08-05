0f63255470a90fd0f8da75f23de1068641b75d40
Joseph McDonough <joseph_mcdonough@colpal.com>
2025-08-05 14:03:49 -0400

    feature: attempting to create a comment with review requested +calling the github api endpoint for adding a comment to the review, with  event type

--- src/index.js
+++ src/index.js
@@ -10,6 +10,29 @@ try {
 
     const payload = JSON.stringify(github.context.payload, undefined, 2);
     core.info(`The event payload: ${payload}`);
+
+    let commitId = process.env.GITHUB_SHA || github.context.sha;
+    let body = "Test Body";
+    let event = "REQUEST_CHANGES";
+    const commentBody = {
+        "commit_id": commitId,
+        "body": body,
+        "event": event
+    }
+
+    try {
+        const response = await fetch(`https://api.github.com/repos/${github.context.repo.owner}/${github.context.repo.repo}/pulls/${github.context.payload.pull_request.number}/reviews`, {
+            method: "POST",
+            headers: {
+                "Authorization": `Bearer ${process.env.GITHUB_TOKEN}`,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(commentBody)
+        });
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+
 } catch (error) {
     core.setFailed(error.message);
 }

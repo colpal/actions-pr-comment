d10d5b676338bd7add27072fb9b0d9276c3b5013
Joseph McDonough <joseph_mcdonough@colpal.com>
2025-10-07 14:14:12 -0400

    feature: don't create initial comment if success and on-resolution-hide + test-comment.yaml -> adjusting test case name to make it clear if comment is visible when it shouldn't be + README.md -> updating changelog, noting the changes in the on-resolution-hide section + comment-workflow -> updated to not post comment if initial comment success and on-resolution-hide + tests/ updated to reflect changes + package... updated to reflect version 0.5.0

--- src/comment/comment-workflow.js
+++ src/comment/comment-workflow.js
@@ -44,14 +44,12 @@ async function commentWorkflow(token) {
             if (conclusion === 'skipped') {
                 logger.debug("Conclusion is 'skipped' and no existing comment found, skipping comment workflow.");
                 return;
-            } else {
+            } else if (core.getInput('on-resolution-hide', { required: false }) === 'true' && conclusion === 'success') {
+                logger.debug("New comment not posted due to success conclusion and on-resolution-hide being true.");
+            }
+            else {
                 logger.debug("No existing comment found, posting a new comment.");
                 comment = await postComment(octokit, owner, repo, commentIdentifier, conclusionIdentifier);
-
-                if (core.getInput('on-resolution-hide', { required: false }) === 'true' && conclusion === 'success') {
-                    logger.debug("New comment hidden as RESOLVED due to success conclusion.");
-                    await hideComment(token, comment, "RESOLVED");
-                }
             }
 
         } else {
